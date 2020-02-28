233c7392eada45b90b80a05a1a5fdab65a2ea90d
Hayk <59624793+hayk24@users.noreply.github.com>
2020-02-28 15:13:43 -0800

    P2

---
+++ src/App/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className='Footer'>
+            Footer
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ src/App/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='Header'>
+            Header
+        </header>
+    )
+};
+
+export default Header;
