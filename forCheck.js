5baaef07b806f5abf4959cd45eb8f3080836c11d
haykpash <24hayk24@gmail.com>
2020-10-27 18:32:35 -0700

    000

---
+++ forCheck.js
@@ -0,0 +1,11 @@
+// // require('dotenv').config();
+// const path = require('path');
+// const express = require('express');
+
+// // Initiaize Instance of Express as app
+// const app = express();
+
+// // POST Requests: In order to accept post requests, you must use bodyParser
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
